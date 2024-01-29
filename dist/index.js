25993dcdd625ae3d14cfa1df185a813396f6749e
yukidayo31 <faizahsiti888@gmail.com>
2024-01-30 02:45:36 +0700

    all http method added

--- dist/index.js
+++ dist/index.js
@@ -6,8 +6,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const connection_1 = __importDefault(require("./connection"));
 const response_1 = __importDefault(require("./response"));
+const body_parser_1 = __importDefault(require("body-parser"));
 const app = (0, express_1.default)();
 const port = 3000;
+app.use(body_parser_1.default.json());
 app.get("/", (req, res) => {
     (0, response_1.default)(200, "this is data", "OK, homepage success!", res);
     console.log("GET homepage, OK!");
@@ -33,6 +35,40 @@ app.get("/mahasiswa/:nim", (req, res) => {
         }
         (0, response_1.default)(200, result, `Successfully GET mahasiswa nim ${nim}!`, res);
         console.log(`GET mahasiswa nim ${nim} data, OK!`);
+    });
+});
+app.post("/mahasiswa", (req, res) => {
+    const { nim, namaLengkap, kelas, alamat } = req.body;
+    const sql = `INSERT INTO mahasiswa (nim, nama_lengkap, kelas, alamat) VALUES (${nim}, '${namaLengkap}', '${kelas}', '${alamat}')`;
+    connection_1.default.query(sql, (err, result) => {
+        if (err) {
+            (0, response_1.default)(500, "Invalid data", "Server error", res);
+            console.log("SERVER ERROR");
+        }
+        if (result === null || result === void 0 ? void 0 : result.affectedRows) {
+            const data = {
+                isSuccess: result.affectedRows,
+                id: result.insertId,
+                details: req.body,
+            };
+            (0, response_1.default)(200, data, `Successfully ADD mahasiswa nim ${nim}!`, res);
+            console.log(`ADD mahasiswa nim ${nim} data, OK!`);
+        }
+        else {
+            (0, response_1.default)(404, "user not found", "data doesn't exist", res);
+            console.log(`Failed to ADD mahasiswa nim ${nim} data`);
+        }
+    });
+});
+app.put("/mahasiswa", (req, res) => {
+    const { nim, namaLengkap, kelas, alamat } = req.body;
+    const sql = `UPDATE mahasiswa SET nama_lengkap = '${namaLengkap}', kelas = '${kelas}', alamat = '${alamat}' WHERE nim = ${nim}`;
+    connection_1.default.query(sql, (err, result) => {
+        if (err) {
+            (0, response_1.default)(500, "Invalid data", "Server error", res);
+            console.log("SERVER ERROR");
+        }
+        console.log(result);
     });
 });
 app.listen(port, () => {
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express, { Express, Request, Response } from "express";
 import db from "./connection";
-import { QueryError, QueryOptions } from "mysql2";
+import { QueryError, ResultSetHeader } from "mysql2";
 import response from "./response";
 import bodyParser from "body-parser";
 
@@ -18,7 +18,7 @@ app.get("/", (req: Request, res: Response) => {
 interface IMahasiswa {
   id: Number;
   nim: Number;
-  nama_lengkap: String;
+  namaLengkap: String;
   kelas: String;
   alamat: String;
 }
@@ -54,8 +54,7 @@ app.post("/mahasiswa", (req: Request, res: Response) => {
   const { nim, namaLengkap, kelas, alamat } = req.body;
   const sql: string = `INSERT INTO mahasiswa (nim, nama_lengkap, kelas, alamat) VALUES (${nim}, '${namaLengkap}', '${kelas}', '${alamat}')`;
 
-  db.query(sql, (err: QueryError, result: any) => {
-    console.log(result);
+  db.query(sql, (err: QueryError, result: ResultSetHeader) => {
     if (err) {
       response(500, "Invalid data", "Server error", res);
       console.log("SERVER ERROR");
@@ -71,6 +70,156 @@ app.post("/mahasiswa", (req: Request, res: Response) => {
     } else {
       response(404, "user not found", "data doesn't exist", res);
       console.log(`Failed to ADD mahasiswa nim ${nim} data`);
+    }
+  });
+});
+
+app.put("/mahasiswa", (req: Request, res: Response) => {
+  const { nim, namaLengkap, kelas, alamat } = req.body;
+  const sql: string = `UPDATE mahasiswa SET nama_lengkap = '${namaLengkap}', kelas = '${kelas}', alamat = '${alamat}' WHERE nim = ${nim}`;
+
+  db.query(sql, (err: QueryError, result: any) => {
+    if (err) {
+      response(500, "Invalid data", "Server error", res);
+      console.log("SERVER ERROR");
+    }
+    if (result?.affectedRows) {
+      const data = {
+        isSuccess: result.affectedRows,
+        info: result.info,
+        details: req.body,
+      };
+      response(200, data, `Successfully UPDATE mahasiswa nim ${nim}!`, res);
+      console.log(`UPDATE mahasiswa nim ${nim} data, OK!`);
+    } else {
+      response(404, "user not found", "data doesn't exist", res);
+      console.log(`Failed to UPDATE mahasiswa nim ${nim} data`);
+    }
+  });
+});
+
+app.delete("/mahasiswa", (req: Request, res: Response) => {
+  const { nim } = req.body;
+  const sql: string = `DELETE FROM mahasiswa WHERE nim = ${nim}`;
+
+  db.query(sql, (err: QueryError, result: ResultSetHeader) => {
+    if (err) {
+      response(500, "Invalid data", "Server error", res);
+      console.log("SERVER ERROR");
+    }
+    if (result?.affectedRows) {
+      const data = {
+        isSuccess: result.affectedRows,
+        details: req.body,
+      };
+      response(200, data, `Successfully DELETE mahasiswa nim ${nim}!`, res);
+      console.log(`DELETE mahasiswa nim ${nim} data, OK!`);
+    } else {
+      response(404, "user not found", "data doesn't exist", res);
+      console.log(`Failed to DELETE mahasiswa nim ${nim} data`);
+    }
+  });
+});
+
+/*          DOSEN          */
+interface IDosen {
+  id: Number;
+  nip: Number;
+  namaLengkap: String;
+  mataKuliah: String;
+  alamat: String;
+}
+
+app.get("/dosen", (req: Request, res: Response) => {
+  const sql: string = "SELECT * FROM dosen";
+
+  db.query(sql, (err: QueryError, result: IDosen[]) => {
+    if (err) {
+      response(500, "Invalid data", "Server error", res);
+      console.log("SERVER ERROR");
+    }
+    response(200, result, "Successfully GET all dosen list!", res);
+    console.log("GET all dosen list, OK!");
+  });
+});
+
+app.get("/dosen/:nip", (req: Request, res: Response) => {
+  const nip: String = req.params.nip;
+  const sql = `SELECT * FROM dosen WHERE nip = ${nip}`;
+
+  db.query(sql, (err: QueryError, result: IDosen[]) => {
+    if (err) {
+      response(500, "Invalid data", "Server error", res);
+      console.log("SERVER ERROR");
+    }
+    response(200, result, `Successfully GET dosen nip ${nip}!`, res);
+    console.log(`GET dosen nip ${nip} data, OK!`);
+  });
+});
+
+app.post("/dosen", (req: Request, res: Response) => {
+  const { nip, namaLengkap, mataKuliah, alamat } = req.body;
+  const sql: string = `INSERT INTO dosen (nip, nama_lengkap, mata_kuliah, alamat) VALUES (${nip}, '${namaLengkap}', '${mataKuliah}', '${alamat}')`;
+
+  db.query(sql, (err: QueryError, result: ResultSetHeader) => {
+    if (err) {
+      response(500, "Invalid data", "Server error", res);
+      console.log("SERVER ERROR");
+    }
+    if (result?.affectedRows) {
+      const data = {
+        isSuccess: result.affectedRows,
+        id: result.insertId,
+        details: req.body,
+      };
+      response(200, data, `Successfully ADD dosen nip ${nip}!`, res);
+      console.log(`ADD dosen nip ${nip} data, OK!`);
+    } else {
+      response(404, "user not found", "data doesn't exist", res);
+      console.log(`Failed to ADD dosen nip ${nip} data`);
+    }
+  });
+});
+
+app.put("/dosen", (req: Request, res: Response) => {
+  const { nip, namaLengkap, mataKuliah, alamat } = req.body;
+  const sql: string = `UPDATE dosen SET nama_lengkap = '${namaLengkap}', mata_kuliah = '${mataKuliah}', alamat = '${alamat}' WHERE nip = ${nip}`;
+
+  db.query(sql, (err: QueryError, result: ResultSetHeader) => {
+    if (result?.affectedRows) {
+      const data = {
+        isSuccess: result.affectedRows,
+        info: result.info,
+        details: req.body,
+      };
+      response(200, data, `Successfully UPDATE dosen nip ${nip}!`, res);
+      console.log(`UPDATE dosen nip ${nip} data, OK!`);
+    } else {
+      response(404, "user not found", "data doesn't exist", res);
+      console.log(`Failed to UPDATE dosen nip ${nip} data`);
+    }
+  });
+});
+
+app.delete("/dosen", (req: Request, res: Response) => {
+  const { nip } = req.body;
+  const sql: string = `DELETE FROM dosen WHERE nip = ${nip}`;
+
+  db.query(sql, (err: QueryError, result: ResultSetHeader) => {
+    if (err) {
+      response(500, "Invalid data", "Server error", res);
+      console.log("SERVER ERROR");
+    }
+    if (result?.affectedRows) {
+      const data = {
+        isSuccess: result.affectedRows,
+        details: req.body,
+      };
+      response(200, data, `Successfully DELETE dosen nip ${nip}!`, res);
+      console.log(`DELETE dosen nip ${nip} data, OK!`);
+    } else {
+      response(404, "user not found", "data doesn't exist", res);
+      console.log(`Failed to DELETE dosen nip ${nip} data`);
     }
   });
 });
